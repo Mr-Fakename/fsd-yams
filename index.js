74136a35577fc338abe8f7685ec190d3d7e2fecd
Mr-Fakename <58395707+Mr-Fakename@users.noreply.github.com>
2022-12-13 12:49:15 +0100

    Add .env + DB connection

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
-const express = require('express');
+// Setup
+const express = require("express");
 const app = express();
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-//Routes
-app.use('/', require('./routes/login'));
-app.use('', require('./routes/index'))
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
+
+// Mongo DB connection
+const database = process.env.MONGOLAB_URI;
+mongoose.connect(database, {useUnifiedTopology: true, useNewUrlParser: true})
+    .then(() => console.log("Connected to the DB"))
+    .catch(err => console.log(err));
+
+// Routes
+app.use("/", require("./routes/login"));
+app.use("", require("./routes/index"))
 const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, console.log("Server running on port " + PORT))
